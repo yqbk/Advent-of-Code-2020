c8f8f805868848b8016902797a7ab94beeaf01ad
Jakub Syrek <jakub.syrek@ynd.co>
2020-12-10 10:32:49 +0100

    Day 3 part 2

--- day3.js
+++ day3.js
@@ -4,31 +4,37 @@ const { readFileSync } = require("fs");
 
 const filename = "day3.input.txt";
 
-const lines = readFileSync(filename)
+const map = readFileSync(filename)
   .toString()
   .replace(/\r\n/g, "\n")
   .split("\n")
 
 
-const slope = [3, 1]
-const mapWidth = lines[0].length
+const mapWidth = map[0].length
 
-const findPath = (currentPosition, treesEncountered) => {
-    console.log('treesEncountered', treesEncountered)
-
+const findPath = (slope, currentPosition = [0,0], treesEncountered = 0) => {
     const newPosition = [(currentPosition[0] + slope[0]) % mapWidth, currentPosition[1] + slope[1]]
-
-    const hasEncounteredTree = lines[newPosition[1]][newPosition[0]] === '#'
+    const hasEncounteredTree = map[newPosition[1]][newPosition[0]] === '#'
 
     if (hasEncounteredTree) {
         treesEncountered += 1
     }
 
-    if (newPosition[1] < lines.length && newPosition[0] < lines[newPosition[1]].length) {
-        return findPath(newPosition, treesEncountered)
+    if (newPosition[1] + slope[1] < map.length && newPosition[0] < map[newPosition[1]].length) {
+        return findPath(slope, newPosition, treesEncountered)
     }
 
     return treesEncountered
 }
 
-console.log(findPath([0,0], 0))
+const possibleSlopes = [[1,1], [3,1], [5,1], [7,1], [1,2]]
+
+
+const result = possibleSlopes.reduce((acc,curr) => findPath(curr) * acc, 1)
+
+console.log('part 1', findPath([3,1]))
+console.log('part 2', result)
+
+
+// const trees = ([x, y = 1]) => 
+//   map.filter((row, i) => (map[i * y] || "")[(i * x) % row.length] === "#").length;
