7e638390727e35e2166c071a355e59edfa595c9d
Jakub Syrek <jakub.syrek@ynd.co>
2020-12-30 09:43:22 +0100

    Day 6 part 2.

--- day6.js
+++ day6.js
@@ -1,5 +1,6 @@
 // https://adventofcode.com/2020/day/6
 
+const { group } = require("console");
 const { readFileSync } = require("fs");
 
 const filename = "day6.input.txt";
@@ -20,11 +21,33 @@ const countCustoms = (group) => {
   return uniqueCustoms.size;
 };
 
-
 // Part 1 answer
 const allGroupsCustoms = groups.reduce(
   (allCustoms, group) => allCustoms + countCustoms(group),
   0
 );
 
-console.log(allGroupsCustoms);
+console.log('Part 1 answer:', allGroupsCustoms);
+
+const countSharedCustoms = (group) => {
+  const people = group.split("\n");
+  let commonCustoms = new Set(people[0].split(""));
+
+  people.forEach((person) => {
+    const personCustoms = person.split("")
+    const personUniqueCustoms = new Set(personCustoms)
+
+    commonCustoms = new Set([...commonCustoms].filter(custom => personUniqueCustoms.has(custom)))
+  });
+
+  return commonCustoms.size;
+};
+
+
+// Part 2 answer
+const allGroupsSharedCustoms = groups.reduce(
+  (allSharedCustoms, group) => allSharedCustoms + countSharedCustoms(group),
+  0
+);
+
+console.log('Part 2 answer:', allGroupsSharedCustoms);
